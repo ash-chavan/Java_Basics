c12e66935a24194b8642a318b34ca10df3e80d5e
ash-chavan <ashwinichavan2103@gmail.com>
2020-08-05 11:25:29 +0530

    Adding to new repo

---
+++ BasicConcept/src/test/java/Selenium/BasicConcept/AlertExample.java
@@ -0,0 +1,27 @@
+package Selenium.BasicConcept;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertExample {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "E:\\TestingJars\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.browserstack.com/users/sign_up");
+		driver.findElement(By.xpath("//input[@id='user_full_name']")).sendKeys("Ashwifvcv");
+		driver.findElement(By.xpath("//input[@id='user_email_login']")).sendKeys("ashwi@gmail.com");
+		driver.findElement(By.xpath("//input[@id='user_password']")).sendKeys("cbdsffDFv");
+		Thread.sleep(4000);
+		driver.findElement(By.xpath("//input[@id='user_submit']")).click(); 
+		System.out.println("user made some changes");
+		System.out.println("user 2 made some changes");
+
+
+
+	}
+
+}
